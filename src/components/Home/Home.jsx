6bb443b528203622072351f00bf45263c1b4e0e9
shivam <shivamkrr986@gmail.com>
2025-11-27 13:24:21 +0530

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,9 +8,14 @@ import NriImage from "../../assets/img/nri/real-estate-investment-india-940x560-
 import LocalityImage from "../../assets/img/locality-insight/borivali-east-or-west-940x559-1-768x457.jpg";
 import LocalityImage1 from "../../assets/img/locality-insight/cost-of-living-in-bangalore-940x560-1-768x458.jpg";
 import InsuranceImage from "../../assets/img/insurance/ChatGPT-Image-Sep-18-2025-01_40_52-PM.png";
-import PropertyImage from "../../assets/img/property-news/secproertynews-300x156.png";
-
-
+import PropertyNImage from "../../assets/img/property-news/secproertynews-300x156.png";
+import InteriorImage from "../../assets/img/interior/b248650c-ec4c-4fca-a7d7-67bafe10a1a4.png";
+import PropertyN1Image from "../../assets/img/property-news/866ac4fce19296b07dbe9198820cbbd1w-c586980953srd-w928_q80.png";
+import PropertyBImage from "../../assets/img/property-buying/Bhk-Cover-image.jpg";
+import ConnectivityImage from "../../assets/img/connectivity/versova-metro.png";
+import Interior1Image from "../../assets/img/interior/How-to-Make-Your-Apartment-Smell-Good.png";
+import DutyCalImage from "../../assets/img/stamp-duty-calculatore-300x179.jpg";
+import AreaCalImage from "../../assets/img/area-calculatore-300x179.jpg";
 
 export default function Home() {
     return (
@@ -236,11 +241,12 @@ export default function Home() {
                         <div className="container">
                             <h3 className="big-article-title py-2">Letest Blogs</h3>
                             <div className="row">
+                                {/*-------- property news ---------*/}
                                 <div className="col-md-4">
                                     <Link to="/article/mannat" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={PropertyImage}
+                                                src={PropertyNImage}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
@@ -249,139 +255,146 @@ export default function Home() {
                                                     What Does a Fed Rate Cut Mean for Mortgage Rates?
                                                 </h6>
                                                 <p className="small-article-date">November 15, 2025</p>
-                                                <p>In response to rising labour market warning signs…</p>
+                                                <p>In response to rising labour market warning signs, the Federal Reserve is poised to lower its benchmark…</p>
                                             </div>
                                         </div>
                                     </Link>
                                 </div>
+                                {/*----------- interior--------- */}
                                 <div className="col-md-4">
                                     <Link to="/article/borivali" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={LocalityImage}
+                                                src={InteriorImage}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
                                             <div className="card-body">
                                                 <h6 className="small-article-title">
-                                                    Borivali East or West, Which Is the Best?
+                                                    Best 3 BHK House Plans: 20+ Designs for Modern Homes
                                                 </h6>
                                                 <p className="small-article-date">November 15, 2025</p>
-                                                <p>Mumbai is a rapidly growing metropolis, with many areas increasing faster than…</p>
+                                                <p>A three-bedroom house plan seamlessly integrates efficiency and…</p>
 
                                             </div>
                                         </div>
                                     </Link>
                                 </div>
+                                {/*-------- property news ---------*/}
                                 <div className="col-md-4">
                                     <Link to="/article/bangalore-cost" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={LocalityImage1}
+                                                src={PropertyN1Image}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
                                             <div className="card-body">
                                                 <h6 className="small-article-title">
-                                                    Cost of Living in Bangalore (2025-2026): Detailed list of monthly expenses
+                                                    Jennifer Lawrence quietly offloads her Beverly Hills home
                                                 </h6>
                                                 <p className="small-article-date">November 20, 2025</p>
-                                                <p>October 15, 2025 Bangalore, dubbed the ‘Silicon Valley of India’, is home…</p>
+                                                <p>Actress Jennifer Lawrence is renowned for strictly guarding her privacy…</p>
 
                                             </div>
                                         </div>
                                     </Link>
                                 </div>
+                                {/*-------- property Buying ---------*/}
                                 <div className="col-md-4">
                                     <Link to="/article/insurance-cost" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={InsuranceImage}
+                                                src={PropertyBImage}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
                                             <div className="card-body">
                                                 <h6 className="small-article-title">
-                                                    The Average Home Insurance Cost in the U.S. for 2025
+                                                   A Comprehensive Guide to BHK: Its Complete Form,Significance, and Elements
                                                 </h6>
                                                 <p className="small-article-date">November 15, 2025</p>
-                                                <p>The average annual cost of homeowners insurance in the United States is…</p>
+                                                <p>In India, describing your house in terms of BHK rather than real square…</p>
 
                                             </div>
                                         </div>
                                     </Link>
                                 </div>
+                                {/*-------- Connectivity ---------*/}
                                 <div className="col-md-4">
                                     <Link to="/article/nri-real-estate" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={NriImage}
+                                                src={ConnectivityImage}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
                                             <div className="card-body">
                                                 <h6 className="small-article-title">
-                                                    Why Avoiding Indian Real Estate Could Be Your Biggest Financial Mistake?
+                                                   Mumbai Versova Ghatkopar Metro Route: Map, Stations & More | Mumbai Line 1 – Blue Line
                                                 </h6>
                                                 <p className="small-article-date">November 20, 2025</p>
-                                                <p>For many Non-Resident Indians (NRIs) and international investors, the Indian real estate…</p>
+                                                <p>The Versova Ghatkopar Metro Route (also known as Mumbai Metro Line 1)…</p>
 
                                             </div>
                                         </div>
                                     </Link>
                                 </div>
+                                {/*-------- interior ---------*/}
                                 <div className="col-md-4">
                                     <Link to="/article/borivali" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={LocalityImage}
+                                                src={Interior1Image}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
                                             <div className="card-body">
                                                 <h6 className="small-article-title">
-                                                    Borivali East or West, Which Is the Best?
+                                                    How to Make Your Apartment Smell Good
                                                 </h6>
                                                 <p className="small-article-date">November 15, 2025</p>
-                                                <p>Mumbai is a rapidly growing metropolis, with many areas increasing faster than…</p>
+                                                <p>Your home should not only look beautiful, but also smell great! A fresh…</p>
 
                                             </div>
                                         </div>
                                     </Link>
                                 </div>
+                                {/*-------- duty Calculator ---------*/}
                                 <div className="col-md-4">
                                     <Link to="/article/bangalore-cost" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={LocalityImage1}
+                                                src={DutyCalImage}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
                                             <div className="card-body">
                                                 <h6 className="small-article-title">
-                                                    Cost of Living in Bangalore (2025-2026): Detailed list of monthly expenses
+                                                    Stamp Duty Calculator
                                                 </h6>
                                                 <p className="small-article-date">November 20, 2025</p>
-                                                <p>October 15, 2025 Bangalore, dubbed the ‘Silicon Valley of India’, is home…</p>
+                                                <p>How to Calculate Stamp Duty in Maharashtra? Stamp duty is a government…</p>
 
                                             </div>
                                         </div>
                                     </Link>
                                 </div>
+                                 {/*-------- area Calculator ---------*/}
                                 <div className="col-md-4">
                                     <Link to="/article/insurance-cost" className="article-link">
                                         <div className="small-article card1 mb-4">
                                             <img
-                                                src={InsuranceImage}
+                                                src={AreaCalImage}
                                                 className="card-img-top big-article-img"
                                                 alt="Featured"
                                             />
                                             <div className="card-body">
                                                 <h6 className="small-article-title">
-                                                    The Average Home Insurance Cost in the U.S. for 2025
+                                                    Area Calculator
                                                 </h6>
                                                 <p className="small-article-date">November 15, 2025</p>
-                                                <p>The average annual cost of homeowners insurance in the United States is…</p>
+                                                <p>A land area calculator is a useful tool for converting one unit to another…</p>
 
                                             </div>
                                         </div>
